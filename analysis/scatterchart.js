14744287d2192da531bab534cc5a134af6035cde
ndshen <andyshen1222@gmail.com>
2017-01-12 15:49:57 +0800

    d

--- analysis/scatterchart.js
+++ analysis/scatterchart.js
@@ -116,8 +116,8 @@ var colors = {
     var pos  = { x: +point.attr("cx"), y: +point.attr("cy") };
     d3.select("#scatTooltip")
       .style("display", "block")
-      .style("left", (pos.x + 30) + "px")
-      .style("top",  (pos.y + pad.top - 10)  + "px")
+      .style("left", (pos.x ) + "px")
+      .style("top",  (pos.y )  + "px")
       .select(".value")
       .text(id);
   };
